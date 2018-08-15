6013773c636ac493867950f281a1cfa1f721a320
awetee <awedupe07@gmail.com>
2018-08-15 17:02:18 +0100

    Add Tags to groups

--- Retros.Domain/Group.cs
+++ Retros.Domain/Group.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Retros.Domain
 {
@@ -12,13 +13,22 @@ namespace Retros.Domain
             this.Name = name;
         }
 
+        protected string _tags;
         public string Name { get; protected set; }
         public Retro Retro { get; protected set; }
         public ICollection<Comment> Comments { get; protected set; } = new List<Comment>();
+        public ICollection<string> Tags => _tags.Split(',');
 
         internal void AddComment(Comment comment)
         {
             this.Comments.Add(comment);
+        }
+
+        internal void AddTag(string tagName)
+        {
+            if (this._tags.Split(',').Any(t => t == tagName))
+                return;
+            this._tags = $"{_tags},{tagName}";
         }
     }
 }
