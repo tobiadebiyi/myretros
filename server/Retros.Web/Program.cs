42feb077ad91e708223501fd5bbebab1759039bd
Oluwatobi Adebiyi <awedupe07@gmail.com>
2019-08-08 22:49:17 +0100

    Set port via $PORT

--- server/Retros.Web/Program.cs
+++ server/Retros.Web/Program.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Retros.Web
 {
     public class Program
     {
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args) {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+            var url = String.Concat("http://0.0.0.0:", port);
+
+           return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(url)
                 .Build();
+        }
 
         public static void Main(string[] args)
         {
